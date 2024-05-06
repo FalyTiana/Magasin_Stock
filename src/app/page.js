f890adfff9fc46ad223b35229edccd9571294b99
FalyTiana <jfalytiana@gmail.com>
2024-05-06 13:50:51 +0300

    first Accueil

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+"use client"
 import Image from "next/image";
 import { BsShop } from "react-icons/bs";
 import { HiOutlineMail } from "react-icons/hi";
@@ -5,8 +6,25 @@ import { CiLock } from "react-icons/ci";
 import bgHome from "@/images/empty-store-front-vector.jpg";
 import styles from "./home.module.css";
 import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState(false)
+  const router = useRouter()
+  const onSubmit = (e) =>{
+    e.preventDefault()
+    if (email.trim() && password.trim()){
+      router.push('/accueil')
+      setError(false)
+      setEmail("")
+      setPassword("")
+    }else{
+     setError(true)
+    }
+  }
   return (
     <>
       <div className={styles.container}>
@@ -17,12 +35,14 @@ export default function Home() {
           <p className={styles.welcome}>Bienvenu chez Nous</p>
           <hr />
           <p className={styles.message}>Connectez vous</p>
-          <form className={styles.form}>
+          <form className={styles.form} onSubmit={onSubmit}>
               <input
                 type="text"
                 id="email"
                 name="email"
                 placeholder="Veuillez entrer votre email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
                 className={styles.input}
               />
               <div className={styles.iconContainer}>
@@ -33,12 +53,14 @@ export default function Home() {
                 name="password"
                 id="password"
                 placeholder="Veuillez entrer votre mot de passe"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
                 className={styles.input}
               />
               <div className={styles.iconContainer}>
                 <CiLock className={styles.inputIcon}/>
               </div>
-            <button className={styles.button} type="button">
+              {error && <p className={styles.error}>Veuillez remplir tous les champs.</p>}            <button className={styles.button} type="submit">
               Accéder à votre magasin
             </button>
           </form>
